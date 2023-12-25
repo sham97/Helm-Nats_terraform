9c6ff814995c37ea96f86af5700983c967b41c3b
sham97 <43527565+sham97@users.noreply.github.com>
2023-12-25 19:53:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
+#####
+# NATS metrics Monitoring CRD
+#####
+module "monitoring_crd"{
+  source = "./service-monitor-crd"
+}
+##########
+# NATS
+############
 module "helm_nats" {
  
   source                    = "./nats-resource"
   values_nats               = [file("./nats-resource/values.yaml")]
-  # values_nats_prom_exporter = [file("../../../../modules/k8s/helm/nats/prometheus-nats-exporter/values.yaml")]
   namespace                 = "hey-apply-your-namespace"
-  depends_on                = [module.k8s_namespaces,module.monitoring_crd]
-
-
+  depends_on                = [module.monitoring_crd]
+  version = "if you need"
 }
